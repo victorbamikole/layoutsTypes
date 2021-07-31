1e7ff5dc56851aa46338f5e81705fe6bf2a9806a
victorbamikole <victorbamikole92@gmail.com>
2021-07-31 02:51:15 +0100

    first commit

---
+++ app/src/main/java/com/example/assignment1/LinearLayout1.kt
@@ -0,0 +1,11 @@
+package com.example.assignment1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayout1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout1)
+    }
+}
